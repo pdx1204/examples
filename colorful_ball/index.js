f1d766df3b683d9fda6787ab4bdb924dd175de0d
qiuqfang <2312180277@qq.com>
2023-02-08 00:01:07 +0800

    chore: 修改样式

--- colorful_ball/index.js
+++ colorful_ball/index.js
@@ -8,36 +8,42 @@ mycanvas.height = document.documentElement.clientHeight;
 let ctx = mycanvas.getContext("2d");
 
 let ballArr = [];
-document.onmousemove = throttleByTimer(function (e) {
-    new SmallBall(e.clientX, e.clientY);
-    //当小球个数大于20时，清屏并删除第一个小球
-    if (ballArr.length > 20) {
-        ctx.clearRect(0, 0, mycanvas.width, mycanvas.height);
-        ballArr.shift();
-    }
-    for (let i in ballArr) {
-        ballArr[i].drawBall();
-    }
-}, 10);
+document.onmousemove = function (e) {
+  new SmallBall(e.clientX, e.clientY);
+  //当小球个数大于20时，清屏并删除第一个小球
+  if (ballArr.length > 20) {
+    ctx.clearRect(0, 0, mycanvas.width, mycanvas.height);
+    ballArr.shift();
+  }
+  for (let i in ballArr) {
+    ballArr[i].drawBall();
+  }
+};
 
 //创建一个小球类
 function SmallBall(x, y) {
-    this.x = x;
-    this.y = y;
-    this.r = parseInt(Math.random() * 50 + 1);
-    ballArr.push(this);
+  this.x = x;
+  this.y = y;
+  this.r = parseInt(Math.random() * 50 + 1);
+  ballArr.push(this);
 }
 
 SmallBall.prototype = {
-    constructor: SmallBall,
-    drawBall: function () {
-        ctx.beginPath();
-        ctx.arc(this.x, this.y, this.r, 0, Math.PI * 2, false);
-        ctx.closePath();
-        ctx.fillStyle = "rgb(" + parseInt(Math.random() * 256) + "," +
-            parseInt(Math.random() * 256) + "," + parseInt(Math.random() * 256) + "," + parseInt(Math.random() * 2) + ")";
-        ctx.fill();
-    }
+  constructor: SmallBall,
+  drawBall: function () {
+    ctx.beginPath();
+    ctx.arc(this.x, this.y, this.r, 0, Math.PI * 2, false);
+    ctx.closePath();
+    ctx.fillStyle =
+      "rgb(" +
+      parseInt(Math.random() * 256) +
+      "," +
+      parseInt(Math.random() * 256) +
+      "," +
+      parseInt(Math.random() * 256) +
+      "," +
+      parseInt(Math.random() * 2) +
+      ")";
+    ctx.fill();
+  },
 };
-
-
